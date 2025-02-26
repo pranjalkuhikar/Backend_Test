247cedb33c56fd0a15db15e13877052e2e8f0320
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-26 21:51:42 +0530

    Add Matter.js integration to Text component for animated word effects; update App component structure

--- Amezon/src/App.jsx
+++ Amezon/src/App.jsx
@@ -1,12 +1,22 @@
-import AmazonAPI from "./Components/AmazonAPI";
+// import AmazonAPI from "./Components/AmazonAPI";
 import Text from "./Components/Text";
 
 const App = () => {
   return (
-    <>
-      <AmazonAPI />
-      <Text />
-    </>
+    <div className="h-screen w-full bg-zinc-400">
+      {/* <AmazonAPI /> */}
+      <Text
+        text={`React Bits is a library of animated and interactive React components designed to streamline UI development and simplify your workflow.`}
+        highlightWords={["React", "Bits", "animated", "components", "simplify"]}
+        highlightClass="highlighted"
+        trigger="hover"
+        backgroundColor="transparent"
+        wireframes={false}
+        gravity={0.56}
+        fontSize="2rem"
+        mouseConstraintStiffness={0.9}
+      />
+    </div>
   );
 };
 
