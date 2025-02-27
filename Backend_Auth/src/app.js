a503e24bc47635d2c9c6a3a68934171bc5531ae6
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-27 21:47:11 +0530

    Add user routes and controller for greeting response

--- Backend_Auth/src/app.js
+++ Backend_Auth/src/app.js
@@ -1,11 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use("/", userRoutes);
 
 export default app;
