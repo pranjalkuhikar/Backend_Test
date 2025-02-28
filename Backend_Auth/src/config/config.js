6a5c487f5d7c79d56ce42e373526000043756386
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-28 13:04:14 +0530

    Add user model with JWT authentication and configuration updates

--- Backend_Auth/src/config/config.js
+++ Backend_Auth/src/config/config.js
@@ -5,6 +5,8 @@ dotenv.config();
 const _config = {
   PORT: process.env.PORT || 2000,
   MONGODB_URL: process.env.MONGODB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 };
 
 const config = Object.freeze(_config);
---
+++ Backend_Auth/src/models/user.models.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: [true, "Username already exists"],
+      minlength: [3, "Username must be at least 3 characters long"],
+      maxlength: [30, "Username must be at most 30 characters long"],
+      trim: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email already exists"],
+      trim: true,
+      lowercase: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "public/profile.jpeg",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      maxlength: [30, "Password must be at most 128 characters long"],
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.statics.hashPassword = async (password) => {
+  if (!password) throw new Error("Password is required");
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+userSchema.methods.comparePassword = async (password) => {
+  if (!password) throw new Error("Password is required");
+  if (!this.password) throw new Error("Password is not valid");
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken = async () => {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: config.JWT_EXPIRES_IN,
+    }
+  );
+  return token;
+};
+
+userSchema.statics.verifyToken = (token) => {
+  if (!token) throw new Error("Token is require");
+  return jwt.verify(token, config.JWT_SECRET);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
