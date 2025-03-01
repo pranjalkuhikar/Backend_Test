69981546ebdbba728d3448dc0927241bd9bf88d1
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-01 14:02:56 +0530

    Refactor server initialization to use HTTP server for improved flexibility

--- Backend_Auth/server.js
+++ Backend_Auth/server.js
@@ -1,10 +1,12 @@
 import app from "./src/app.js";
 import config from "./src/config/config.js";
 import connectDB from "./src/db/db.js";
+import http from "http";
 
 connectDB();
 const port = config.PORT;
+const server = http.createServer(app);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
