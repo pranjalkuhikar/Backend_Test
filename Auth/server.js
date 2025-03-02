36951b759fd2626921be4d368704485dfc7b356b
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-02 18:02:59 +0530

    Refactor server configuration to use environment variables and add config module

--- Auth/server.js
+++ Auth/server.js
@@ -1,7 +1,8 @@
 import express from "express";
+import config from "./src/config/config.js";
 
 const app = express();
-const port = 3000;
+const port = config.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
