5515f5906d00bc080c21b2056cde27b7c66bf78a
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-02 23:04:18 +0530

    Add user registration validation middleware

---
+++ Auth/src/middleware/user.middleware.js
@@ -0,0 +1,23 @@
+import { body } from "express-validator";
+
+export const validateRegisterUser = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 1 })
+    .withMessage("Username is required"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please fill a valid email address"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
