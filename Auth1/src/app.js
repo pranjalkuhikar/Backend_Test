f33fdb7cd72e9075638572775deb3c2aff120756
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-03 18:46:22 +0530

    Enhance Express app setup with middleware for JSON parsing, URL encoding, logging, and cookie parsing

--- Auth1/src/app.js
+++ Auth1/src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import apiRoutes from "./routes/user.routes.js";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cookieParser());
 
 app.use("/users", apiRoutes);
 
