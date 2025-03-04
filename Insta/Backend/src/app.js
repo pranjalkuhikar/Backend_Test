e415f19c4c5e79466bbed2d434a5e98cf5c1c7a7
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-04 18:05:47 +0530

    Add CORS support and update package.json dependencies

--- Insta/Backend/src/app.js
+++ Insta/Backend/src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import CRUDAPI from "./routes/user.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use("/", CRUDAPI);
 
 export default app;
