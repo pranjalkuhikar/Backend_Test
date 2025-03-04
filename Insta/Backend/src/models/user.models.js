df2ab1b40769e4fde726632097a802767672087e
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-04 17:32:22 +0530

    Add user model with schema for username, name, image, and caption

---
+++ Insta/Backend/src/models/user.models.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
