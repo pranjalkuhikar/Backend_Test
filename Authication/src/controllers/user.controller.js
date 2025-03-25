32fac84c96ef34d11b59e2d9f861681a80a82c5a
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-25 19:51:58 +0530

    Enhance user registration by adding error handling and checking for existing users

--- Authication/src/controllers/user.controller.js
+++ Authication/src/controllers/user.controller.js
@@ -4,20 +4,30 @@ import bcrypt from "bcrypt";
 import config from "../config/config.js";
 
 export const register = async (req, res) => {
-  const { username, email, password } = req.body;
-  if (!username || !email || !password) {
-    return res.status(400).send("All fields are required");
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).send("All fields are required");
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const alreadyUser = await User.find({ email: email });
+    if (alreadyUser) {
+      return res.status(400).send("User already exists");
+    }
+
+    const user = await User.create({
+      username: username,
+      email: email,
+      password: hashPassword,
+    });
+
+    const token = jwt.sign({ user: user._id }, config.TOKEN_SECRET);
+
+    res.status(201).send({ message: "User registered successfully", token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
   }
-
-  const hashPassword = await bcrypt.hash(password, 10);
-
-  const user = await User.create({
-    username: username,
-    email: email,
-    password: hashPassword,
-  });
-
-  const token = jwt.sign({ user: user._id }, config.TOKEN_SECRET);
-
-  res.send("Welcome to " + user + token);
 };
