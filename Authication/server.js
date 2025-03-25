8800e4059b42617b754c70a9bdf82217e210e88a
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-25 19:04:04 +0530

    Refactor server configuration to use environment variables for port

--- Authication/server.js
+++ Authication/server.js
@@ -1,6 +1,7 @@
 import app from "./src/app.js";
+import config from "./src/config/config.js";
 
-const PORT = 3000;
+const PORT = config.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
