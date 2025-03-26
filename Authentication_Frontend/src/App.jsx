e86985201744fae246b7804b5f60465690afb01f
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-26 14:01:00 +0530

    Add login component and integrate it into the main app

--- Authentication_Frontend/src/App.jsx
+++ Authentication_Frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import Login from "./components/Login";
 import Register from "./components/Register";
 
 const App = () => {
   return (
     <>
       <Register />
+      <Login />
     </>
   );
 };
