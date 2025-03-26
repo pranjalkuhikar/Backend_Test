d0b63a209b89c3c246d2b8d3a85e537cd35b9687
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-26 14:13:13 +0530

    Add routing and navigation for authentication components

--- Authentication_Frontend/src/App.jsx
+++ Authentication_Frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Link, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Register from "./components/Register";
@@ -5,9 +6,39 @@ import Register from "./components/Register";
 const App = () => {
   return (
     <>
-      <Register />
-      <Login />
-      <Profile />
+      <nav className="bg-gray-600 text-white p-4 shadow-md font-[Arial]">
+        <ul className="flex justify-center space-x-6">
+          <li>
+            <Link
+              to={"/register"}
+              className="hover:text-blue-300 transition-colors"
+            >
+              Register
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"/login"}
+              className="hover:text-blue-300 transition-colors"
+            >
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"/profile"}
+              className="hover:text-blue-300 transition-colors"
+            >
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   );
 };
